87855f1febbc4f7b1315e4b129a7bc1608dcd631
Aakash-AA <aakash.aanegola@gmail.com>
2021-05-11 13:58:52 +0530

    Updated palm tree leaf generation

--- leaf.py
+++ leaf.py
@@ -30,48 +30,53 @@ def create_leaf(params):
     for i in range(1, s+1):
         if l*i <= s * c:
             y = l*i/s
-            pos = Vector(((w/2)*sin(radians(90*y/c)), 
-                          y, 
-                          (c**2/(l-c)**2)*d*(sin(radians(90*y/c))))) 
+            x = (w/2)*sin(radians(90*y/c))
+            z = (c**2/(l-c)**2)*d*(sin(radians(90*y/c)))
+            
+            pos = Vector((x, y, z))
         else:
             y = l*i/s
-            pos = Vector(((w/2)*(sin(radians(90)-radians(90*(y-c)/(l-c)))),
-                          y,
-                          (c**2/(l-c)**2)*d-d*(1-cos(radians(90*(y-c)/(l-c))))))
+            x = (w/2)*(sin(radians(90)-radians(90*(y-c)/(l-c))))
+            z = (c**2/(l-c)**2)*d-d*(1-cos(radians(90*(y-c)/(l-c))))
+            
+            pos = Vector((x, y, z))
+            
         bmesh.ops.create_vert(bm, co=pos)
         pos.x *= -1
         bmesh.ops.create_vert(bm, co=pos)
-     
-    #bmesh.ops.contextual_create(bm, geom=bm.verts)
+        pos.z += pos.x/w
+        pos.x = 0
+        bmesh.ops.create_vert(bm, co=pos)
     
     # Add the terminal edges    
     bm.verts.ensure_lookup_table()
     bm.edges.new((bm.verts[0], bm.verts[1]))
     bm.edges.new((bm.verts[0], bm.verts[2]))
-    bm.edges.new((bm.verts[-1], bm.verts[-2]))
-    bm.edges.new((bm.verts[-1], bm.verts[-3]))
-   
+    bm.edges.new((bm.verts[0], bm.verts[3]))
+    
     for i in range(1, len(bm.verts)-3):
         bm.verts.ensure_lookup_table()
-        # If odd add the vertical edge to distinguish segment
-        if i%2 == 1:
+        # Add the vertical edge
+        if i%3 == 1:
+            bm.edges.new((bm.verts[i], bm.verts[i+2]))
+        elif i%3 == 2:
             bm.edges.new((bm.verts[i], bm.verts[i+1]))
         
         # Add the lateral edge
-        bm.edges.new((bm.verts[i], bm.verts[i+2]))
+        bm.edges.new((bm.verts[i], bm.verts[i+3]))
       
-    rot = Matrix.Rotation(-radians(angle), 4, "X")
+    rot = Matrix.Rotation(radians(angle), 4, "X")
     bmesh.ops.rotate(bm, cent=Vector((0.0, 0.0, 0.0)), matrix=rot, verts=bm.verts)         
     
     return bm
 
 
-bm = create_leaf({"length" : 10,
+bm = create_leaf({"length" : 5,
                    "width" : 3,
-                   "center" : 4.5,
+                   "center" : 2,
                    "segments" : 9,
-                   "dropoff" : 6,
-                   "angle" : 10})
+                   "dropoff" : 1,
+                   "angle" : 0})
 
 # Finish up, write the bmesh into a new mesh
 me = bpy.data.meshes.new("Mesh")
