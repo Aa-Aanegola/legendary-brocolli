9a9784c908a23cdf73964988320876ed6e08e889
Aakash Aanegola <38849860+Aa-Aanegola@users.noreply.github.com>
2021-05-12 23:59:20 +0530

    Update leaf.py

--- leaf.py
+++ leaf.py
@@ -20,6 +20,7 @@ def create_leaf(params):
     s = params["segments"]
     d = params["dropoff"]
     angle = params["angle"]
+    thickness = params["thickness"]
     
     bm = bmesh.new()
     
@@ -28,61 +29,67 @@ def create_leaf(params):
     
     # Create all intermediate vertices in pairs
     for i in range(1, s+1):
+        y = l*i/s
         if l*i <= s * c:
-            y = l*i/s
             x = (w/2)*sin(radians(90*y/c))
             z = (c**2/(l-c)**2)*d*(sin(radians(90*y/c)))
             
             pos = Vector((x, y, z))
         else:
-            y = l*i/s
             x = (w/2)*(sin(radians(90)-radians(90*(y-c)/(l-c))))
             z = (c**2/(l-c)**2)*d-d*(1-cos(radians(90*(y-c)/(l-c))))
             
             pos = Vector((x, y, z))
-            
+        
+        p = random.uniform(0.90, 1.1)
+        pos.x *= p
         bmesh.ops.create_vert(bm, co=pos)
-        pos.x *= -1
+        pos.x *= -1/p
+        pos.x *= random.uniform(0.90, 1.1)
         bmesh.ops.create_vert(bm, co=pos)
         pos.z += pos.x/w
         pos.x = 0
         bmesh.ops.create_vert(bm, co=pos)
     
-    # Add the terminal edges    
+    bmesh.ops.remove_doubles(bm, dist=0.001, verts = bm.verts)
+      
+    # Create faces
     bm.verts.ensure_lookup_table()
-    bm.edges.new((bm.verts[0], bm.verts[1]))
-    bm.edges.new((bm.verts[0], bm.verts[2]))
-    bm.edges.new((bm.verts[0], bm.verts[3]))
-    
-    for i in range(1, len(bm.verts)-3):
-        bm.verts.ensure_lookup_table()
-        # Add the vertical edge
-        if i%3 == 1:
-            bm.edges.new((bm.verts[i], bm.verts[i+2]))
-        elif i%3 == 2:
-            bm.edges.new((bm.verts[i], bm.verts[i+1]))
+    for i in range(3, len(bm.verts) - 4, 3):
+        bm.faces.new((bm.verts[i], bm.verts[i-2], bm.verts[i+1], bm.verts[i+3]))
+        bm.faces.new((bm.verts[i], bm.verts[i+3], bm.verts[i+2], bm.verts[i-1]))
         
-        # Add the lateral edge
-        bm.edges.new((bm.verts[i], bm.verts[i+3]))
+    # Terminal faces
+    bm.faces.new((bm.verts[0], bm.verts[1], bm.verts[3]))
+    bm.faces.new((bm.verts[0], bm.verts[3], bm.verts[2]))
+    bm.faces.new((bm.verts[-1], bm.verts[-3], bm.verts[-2]))
+    bm.faces.new((bm.verts[-1], bm.verts[-2], bm.verts[-4]))
       
     rot = Matrix.Rotation(radians(angle), 4, "X")
     bmesh.ops.rotate(bm, cent=Vector((0.0, 0.0, 0.0)), matrix=rot, verts=bm.verts)         
     
-    return bm
+    # Finish up, write the bmesh into a new mesh
+    me = bpy.data.meshes.new("Mesh")
+    bm.to_mesh(me)
+    bm.free()
+
+    # Add the mesh to the scene
+    obj = bpy.data.objects.new("Leaf", me)
+    mod = obj.modifiers.new(f"{obj.name}", "SOLIDIFY")
+    mod.thickness = thickness
+    
+    return obj
 
 
-bm = create_leaf({"length" : 5,
+obj = create_leaf({"length" : 10,
                    "width" : 3,
-                   "center" : 2,
-                   "segments" : 9,
-                   "dropoff" : 1,
-                   "angle" : 0})
+                   "center" : 4,
+                   "segments" : 7,
+                   "dropoff" : 3,
+                   "angle" : 0,
+                   "thickness": 0.1
+                   })
 
-# Finish up, write the bmesh into a new mesh
-me = bpy.data.meshes.new("Mesh")
-bm.to_mesh(me)
-bm.free()
 
-# Add the mesh to the scene
-obj = bpy.data.objects.new("Leaf", me)
+
 bpy.context.collection.objects.link(obj)
