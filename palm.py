97ee95eeb49f229977444505deeeb8d246b8d016
Ishaan Shah <ishaan.n.shah@gmail.com>
2021-05-13 10:23:24 +0530

    Palm tree complete

--- palm.py
+++ palm.py
@@ -5,7 +5,7 @@ import random
 import sys
 import os
 import time
-from math import radians
+from math import radians, sin, cos
 
 from random import randrange
 
@@ -13,14 +13,7 @@ from random import randrange
 bpy.ops.object.select_all(action='SELECT')
 bpy.ops.object.delete(use_global=False, confirm=False)
 
-# Tree Hyperparameters
-height = 20
-base_radius = 1
-end_radius = base_radius * (0.3 + random.uniform(-0.05, 0.05))
-layers = 8
-low_polyness = 6
 seed = time.time()
-
 random.seed(seed)
 
 # TODO: Follow bezier
@@ -32,11 +25,93 @@ def get_center(verts):
     res /= len(verts)
     return res
 
+def create_leaf(params):
+    l = params["length"]
+    w = params["width"]
+    c = params["center"]
+    s = params["segments"]
+    d = params["dropoff"]
+    angle = params["angle"]
+    thickness = params["thickness"]
+    
+    bm = bmesh.new()
+    
+    # Add the left vertex
+    bmesh.ops.create_vert(bm, co=Vector((0, 0, 0)))
+    
+    # Create all intermediate vertices in pairs
+    for i in range(1, s+1):
+        y = l*i/s
+        if l*i <= s * c:
+            x = (w/2)*sin(radians(90*y/c))
+            z = (c**2/(l-c)**2)*d*(sin(radians(90*y/c)))
+            
+            pos = Vector((x, y, z))
+        else:
+            x = (w/2)*(sin(radians(90)-radians(90*(y-c)/(l-c))))
+            z = (c**2/(l-c)**2)*d-d*(1-cos(radians(90*(y-c)/(l-c))))
+            
+            pos = Vector((x, y, z))
+        
+        p = random.uniform(0.90, 1.1)
+        pos.x *= p
+        bmesh.ops.create_vert(bm, co=pos)
+        pos.x *= -1/p
+        pos.x *= random.uniform(0.90, 1.1)
+        bmesh.ops.create_vert(bm, co=pos)
+        pos.z += pos.x/w
+        pos.x = 0
+        bmesh.ops.create_vert(bm, co=pos)
+    
+    bmesh.ops.remove_doubles(bm, dist=0.001, verts = bm.verts)
+      
+    # Create faces
+    bm.verts.ensure_lookup_table()
+    for i in range(3, len(bm.verts) - 4, 3):
+        bm.faces.new((bm.verts[i], bm.verts[i-2], bm.verts[i+1], bm.verts[i+3]))
+        bm.faces.new((bm.verts[i], bm.verts[i+3], bm.verts[i+2], bm.verts[i-1]))
+        
+    # Terminal faces
+    bm.faces.new((bm.verts[0], bm.verts[1], bm.verts[3]))
+    bm.faces.new((bm.verts[0], bm.verts[3], bm.verts[2]))
+    bm.faces.new((bm.verts[-1], bm.verts[-3], bm.verts[-2]))
+    bm.faces.new((bm.verts[-1], bm.verts[-2], bm.verts[-4]))
+      
+    rot = Matrix.Rotation(radians(angle), 4, "X")
+    bmesh.ops.rotate(bm, cent=Vector((0.0, 0.0, 0.0)), matrix=rot, verts=bm.verts)         
+    
+    # Finish up, write the bmesh into a new mesh
+    me = bpy.data.meshes.new("Mesh")
+    bm.to_mesh(me)
+    bm.free()
+
+    # Add the mesh to the scene
+    obj = bpy.data.objects.new("Leaf", me)
+    mod = obj.modifiers.new(f"{obj.name}", "SOLIDIFY")
+    mod.thickness = thickness
+    
+    return obj
+
+# Create Trunk
+# Tree Hyperparameters
+height = 20
+base_radius = 1
+end_radius = base_radius * (0.3 + random.uniform(-0.05, 0.05))
+layers = 8
+low_polyness = 6
+leaf_length = 6
+leaf_width = 1.5
+leaf_dropoff = 1.75
+leaf_count = 9
+leaf_elev_layers = 2 # Can be 1 to 3
+leaf_center = 2
+leaf_segments = 6
+leaf_thickness = 0.1
+
 layer_list = []
 for i in range(0, layers+1):
     layer_list.append(i*height/layers)
 
-# Create Trunk
 bm = bmesh.new()
 
 bmesh.ops.create_circle(
@@ -95,7 +170,6 @@ for i in range(1, layers+1):
     if prev_normal.z < 0:
         prev_normal *= -1
     bmesh.ops.delete(bm, geom=[face], context='FACES')
-    print(get_center(translate_verts), outer_center)
             
     last_edges = [e for e in extruded['geom'] if isinstance(e, bmesh.types.BMEdge)]
     prev_radius = radius
@@ -115,3 +189,30 @@ bm.free()
 # Add the mesh to the scene
 obj = bpy.data.objects.new("Trunk", me)
 bpy.context.collection.objects.link(obj)
+
+    
+# Create leafs
+for i in range(leaf_count):
+    theta = (i * 360 / leaf_count) + (360/leaf_count) * random.uniform(-0.1, 0.1)
+    if i % 2:
+        angle = random.uniform(-10, 0)
+    else:
+        angle = random.uniform(0, 10)
+    leaf = create_leaf({
+        "length": leaf_length*random.uniform(0.9, 1.1),
+        "center": leaf_center*random.uniform(0.8, 1.1),
+        "width": leaf_width*random.uniform(0.9, 1.1),
+        "segments": leaf_segments,
+        "dropoff": leaf_dropoff*random.uniform(0.95, 1.05),
+        "thickness": leaf_thickness,
+        "angle": angle
+    })
+    leaf.rotation_euler = (0, 0, radians(theta))
+    
+    # Translate leaf to top of trunk
+    leaf.location = center
+    
+    # Rotate it to match angle of the trunk
+    leaf.delta_rotation_euler = prev_normal
+    
+    bpy.context.collection.objects.link(leaf)
